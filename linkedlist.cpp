5df09c9fb4d2548ba85c7c9d1de115b1051a9ef5
Sushank Subhash Warde <90682961+SushankSubhashWarade@users.noreply.github.com>
2025-02-01 21:06:35 +0530

    Add files via upload

---
+++ linkedlist.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class Node{
+	public:
+		int data;
+		Node*next;
+		
+		Node(int val){
+			data=val;
+			next=NULL;
+			
+		}
+};
+
+class List{
+	Node* head;
+	Node* tail;
+	
+	public:
+		List(){
+		head= tail=NULL;	
+		}
+		void push_front(int val){
+			Node* newNode = new Node(val);
+		   if(head==NULL){
+		   	head=tail=newNode;
+		   	return;
+		   	
+		   }
+		   else{
+		   	newNode->next = head;
+		   	head=newNode;
+		   }
+		}
+		void printll(){
+			Node* temp = head;
+			
+			{
+				cout<< temp -> data<<" ";
+				temp= temp->next;
+				cout<<endl;
+			}
+		}
+};
+
+int main(){
+List ll;
+	ll.push_front(1);
+	ll.push_front(2);
+	ll.push_front(3);
+	
+	ll.printll();
+	
+	return 0;
+}
